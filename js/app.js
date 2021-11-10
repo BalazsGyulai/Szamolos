cf14b0f0c655be9373c8d8af3910de2884d48796
BalazsGyulai <60194914+BalazsGyulai@users.noreply.github.com>
2021-11-10 21:39:54 +0100

    Bug fixed

--- js/app.js
+++ js/app.js
@@ -61,5 +61,12 @@ function winner(){
 }
 
 function newGame(){
-    num = Math.floor(Math.random() * 1000); 
+    num = Math.floor(Math.random() * 1000);
+    winnerDisplay.style.display = "none";
+    db = 0;
+    var child = showTips.lastElementChild;
+    while (child) {
+       showTips.removeChild(child);
+       child = showTips.lastElementChild;
+    }
 }
