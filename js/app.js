f90e82d78e068dc49acab582927176d5dc884322
BalazsGyulai <trombita0531@gmail.com>
2021-11-14 12:55:48 +0100

    version 1.3 - the files are clear

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+// variables
+
+
 const tip = document.querySelector("#tip");
 const btn = document.querySelector("#submit");
 const showTips = document.querySelector("#showTips");
@@ -10,50 +13,47 @@ let num = 0;
 let db = 1;
 
 
-gameDisplay.style.height = window.innerHeight * 0.9 + 'px';
+// --- variables ---
 
-if (window.innerWidth < 500){
-    gameDisplay.style.width = window.innerWidth * 0.9 + 'px';
-} else{
-    gameDisplay.style.width = '500px'
+
+// functions and classes
+
+
+class CreateThing {
+    constructor(what, which, tipNum, where, text) {
+        this.Element = what;
+        this.NumElement = which;
+        this.TipNum = tipNum;
+        this.ElementAddTo = where;
+        this.Text = text
+    }
+
+    create() {
+        const li = document.createElement(this.Element);
+        li.innerHTML = this.NumElement + this.Text + this.TipNum;
+        this.ElementAddTo.prepend(li);
+    }
 }
 
-
-winnerTexts.style.width = gameDisplay.getBoundingClientRect().width + "px";
-
-showTips.style.height = gameDisplay.offsetHeight - playerArea.offsetHeight - 35 + "px";
-
-newGame();
-
-tip.addEventListener("keyup", function(e) {
-    if (e.keyCode === 13) {
-        checkNumber()
-    }
-})
-
-newGamebtn.addEventListener("click", function() {
- newGame();
-    
-});
-btn.addEventListener("click", checkNumber);
-
 function checkNumber() {
-    if (tip.value == ""){
+    if (tip.value == "") {
         tip.value = 0;
     }
 
-    if(tip.value > num){
-        li = document.createElement("li");
-        li.innerHTML = db + ". A szám kisebb, mint " + tip.value;
-        showTips.prepend(li);
-    } else if(tip.value < num){
-        li = document.createElement("li");
-        li.innerHTML = db + ". A szám nagyobb, mint " + tip.value;
-        showTips.prepend(li);
+    if (tip.value > num) {
+        const createLi = new CreateThing("li", db, tip.value, showTips, ".) A szám kisebb, mint ");
+
+        createLi.create();
+
+    } else if (tip.value < num) {
+        const createLi = new CreateThing("li", db, tip.value, showTips, ". A szám nagyobb, mint ");
+
+        createLi.create();
+
     } else {
-        li = document.createElement("li");
-        li.innerHTML = db + ". A szám nagyobb, mint " + tip.value;
-        showTips.prepend(li);
+        const createLi = new CreateThing("li", db, tip.value, showTips, ".) Kitaláltad a számot: ");
+
+        createLi.create();
         winner();
     }
 
@@ -62,20 +62,66 @@ function checkNumber() {
     db++
 }
 
-function winner(){
+function winner() {
     winnerDisplay.style.display = "flex";
     p = winnerDisplay.querySelector("#texts").getElementsByTagName("p")[0];
     p.innerHTML = "A szám, amire gondoltam a " + num + " volt, amit " + db + " tipp alatt tudtál megfejteni!";
 }
 
-function newGame(){
+function newGame() {
     num = Math.floor(Math.random() * 1000);
     winnerDisplay.style.display = "none";
     db = 1;
 
     var child = showTips.lastElementChild;
     while (child) {
-       showTips.removeChild(child);
-       child = showTips.lastElementChild;
+        showTips.removeChild(child);
+        child = showTips.lastElementChild;
     }
 }
+
+
+// --- functions and classes ---
+
+
+// responsive part
+
+
+gameDisplay.style.height = window.innerHeight * 0.9 + 'px';
+
+if (window.innerWidth < 500) {
+    gameDisplay.style.width = window.innerWidth * 0.9 + 'px';
+} else {
+    gameDisplay.style.width = '500px'
+}
+
+winnerTexts.style.width = gameDisplay.getBoundingClientRect().width + "px";
+
+showTips.style.height = gameDisplay.offsetHeight - playerArea.offsetHeight - 35 + "px";
+
+
+// --- responsive part ---
+
+
+// start the game
+
+newGame();
+
+// --- start the game ---
+
+
+// add events to variables
+
+tip.addEventListener("keyup", function (e) {
+    if (e.keyCode === 13) {
+        checkNumber()
+    }
+})
+
+newGamebtn.addEventListener("click", function () {
+    newGame();
+});
+
+btn.addEventListener("click", checkNumber);
+
+// --- add events to variables ---
